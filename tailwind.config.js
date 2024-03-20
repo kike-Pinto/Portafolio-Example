9eb2489c1efab8376d474bed22e8d873e0d4cd57
kike-pinto <cripintort@gmail.com>
2024-03-20 17:36:53 -0300

    primer commit Portafolio-Example

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}', './index.html', './src/input.css'],
+  theme: {
+    fontFamily: {
+      sans: ['Open Sans', 'sans-serif'],
+      inter: ['Inter', 'sans-serif'],
+      montserrat: ['Montserrat', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        primary: '#0872BF',
+        background: '#141A1A',
+      },
+      dropShadow: {
+        '3xl': '0 4px 20px rgba(93, 173, 236, 0.23)',
+      },
+    },
+  },
+  plugins: [],
+}
